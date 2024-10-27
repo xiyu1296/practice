e8b6fd87a7c363042b4d25f5c7fb446f1879ebe3
xiyu1296 <xiyu1296@gmail.com>
2024-10-27 19:08:25 +0800

    practice_v0.1

---
+++ src/pingtu_game/ui/registerui.java
@@ -0,0 +1,16 @@
+package pingtu_game.ui;
+
+import javax.swing.*;
+
+public class registerui extends JFrame{
+    public registerui(){
+        //注册
+        this.setSize(488,500);
+        this.setTitle("register");
+        this.setAlwaysOnTop(true);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+        this.setVisible(true);
+    }
+}
